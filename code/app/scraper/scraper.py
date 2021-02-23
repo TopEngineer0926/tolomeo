d106857282bc10d436382f904dae89bc284f4056
TopEngineer0926 <top_engineer_0926@outlook.com>
2021-02-24 00:36:27 +0100

    test firefox and chrome with tor proxy

--- code/app/scraper/scraper.py
+++ code/app/scraper/scraper.py
@@ -28,15 +28,15 @@ proxies = {
 }
 
 
-def test_ip_change():
-
-    url = "https://icanhazip.com/" # https://www.whatismyip.com/
-    response = requests.get(url)
-    print_ip(response.text)
+def test_ip_change_requests():
+    logging.info('-----------------Using requests-----------------')
+    url = "https://icanhazip.com/" 
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+    }
     
-    #logging.info('ip: {}'.format(response.text.strip()))
     change_ip()
-    response = requests.get(url, proxies=proxies)
+    response = requests.get(url, proxies=proxies, headers=headers)
     print_ip(response.text)
     old_ip = response.text
     new_ip = old_ip
@@ -46,10 +46,53 @@ def test_ip_change():
         seconds = seconds + 2
         change_ip()
         time.sleep(2)
-        response = requests.get(url, proxies=proxies)
+        response = requests.get(url, proxies=proxies, headers=headers)
         print_ip(response.text)
         new_ip = response.text
     print_ip(new_ip + ' cambiato in secondi:' + str(seconds))
+    logging.info('-----------------Closing requests-----------------')
+
+def test_ip_change_firefox():
+    logging.info('-----------------Using firefox-----------------')
+    url = "https://icanhazip.com/" 
+    
+    change_ip()
+    response = remote_web_driver(url)
+    print_ip(response)
+    old_ip = response
+    new_ip = old_ip
+
+    seconds = 0
+    while old_ip == new_ip:
+        seconds = seconds + 2
+        change_ip()
+        time.sleep(2)
+        response = remote_web_driver(url)
+        print_ip(response)
+        new_ip = response
+    print_ip(new_ip + ' cambiato in secondi:' + str(seconds))
+    logging.info('-----------------Closing firefox-----------------')
+
+def test_ip_change_chrome():
+    logging.info('-----------------Using Chrome-----------------')
+    url = "https://icanhazip.com/" 
+    
+    change_ip()
+    response = remote_web_driver_chrome(url)
+    print_ip(response)
+    old_ip = response
+    new_ip = old_ip
+
+    seconds = 0
+    while old_ip == new_ip:
+        seconds = seconds + 2
+        change_ip()
+        time.sleep(2)
+        response = remote_web_driver_chrome(url)
+        print_ip(response)
+        new_ip = response
+    print_ip(new_ip + ' cambiato in secondi:' + str(seconds))
+    logging.info('-----------------Closing Chrome-----------------')
 
 
 def print_ip(html):
@@ -64,20 +107,45 @@ def change_ip():
     s.send(('AUTHENTICATE "'+P+'"\r\nSIGNAL NEWNYM\r\n').encode())
     s.close()
 
-def test_remote_web_driver():
-    options = Options()
-    options.add_argument('--proxy-server=socks5://proxy:9050')
+#not working ip rotation
+def remote_web_driver(url):
+    options = webdriver.FirefoxOptions()
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument('--headless')
+    options.add_argument('--proxy=socks5://proxy:9050')
 
+    
     driver = webdriver.Remote(
             desired_capabilities=DesiredCapabilities.FIREFOX,
             command_executor="http://firefox-driver:4444",
             options=options
+            #proxy=proxy
         )
-    driver.get('https://the-internet.herokuapp.com')
-    logging.info(driver.title)
-    driver.close()
+    driver.get(url)
+    response = driver.page_source
+    driver.quit()
+    return response
+
+#working ip rotation
+def remote_web_driver_chrome(url):
+    PROXY = 'socks5://proxy:9050'
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--proxy-server=%s' % PROXY)
 
     
+    driver = webdriver.Remote(
+            desired_capabilities=DesiredCapabilities.CHROME,
+            command_executor="http://chrome-driver:4444",
+            options=options
+        )
+    driver.get(url)
+    response = driver.page_source
+    driver.quit()
+    return response
+    
 if __name__ == "__main__":
-    #test_ip_change()
-    #test_remote_web_driver()
+#    test_ip_change_requests()
+    # test_ip_change_firefox()
+    test_ip_change_chrome()
