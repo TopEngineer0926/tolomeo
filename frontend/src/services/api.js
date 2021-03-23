8371235c8bc7bd370e05a04313e0adc8c705915a
TopEngineer0926 <top_engineer_0926@outlook.com>
2021-03-23 05:17:14 -0700

    added the authentication part

---
+++ frontend/src/services/api.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import authHeader from './authHeader';
+const API_URL = process.env.REACT_APP_API_URL;
+class AdminService {
+
+  //Login
+  login(data) {
+    return axios.post(API_URL + 'login', data, {});
+  }
+
+  getEvidences(limit, page) {
+    return axios.get(API_URL + 'evidences?limit=' + limit + '&page=' + page, { headers: authHeader() });
+  }
+
+  getCharts(limit, page) {
+    return axios.get(API_URL + 'map?limit=' + limit + '&page=' + page, { headers: authHeader() });
+  }
+
+}
+
+export default new AdminService();
