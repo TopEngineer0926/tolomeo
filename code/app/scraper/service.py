d6d4b63f71be8d35810422aa73ece5f3bdc64f9d
TopEngineer0926 <top_engineer_0926@outlook.com>
2021-04-02 19:37:33 +0200

    feat: add pagination backend (#14)

--- code/app/scraper/service.py
+++ code/app/scraper/service.py
@@ -1,4 +1,5 @@
 import uuid
+import math
 
 from app.repository import Repository
 from app.repository.postgres import PostgresRepository
@@ -11,12 +12,26 @@ class Service(object):
     def get_evidences(
         self, limit=10, page=1, query_filter="", only_keywords_found=False
     ):
-        return self.repo_client.get_evidences(
-            limit, page, query_filter, only_keywords_found
+        total_data = self.repo_client.get_evidences_count(
+            query_filter, only_keywords_found
         )
+        return {
+            "total_data": total_data,
+            "total_pages": math.ceil(int(total_data) / int(limit) + 0.5),
+            "page": int(page),
+            "items": self.repo_client.get_evidences(
+                limit, page, query_filter, only_keywords_found
+            ),
+        }
 
     def get_evidences_map(self, uuid, limit=10, page=1):
-        return self.repo_client.get_evidences_map(uuid, limit, page)
+        total_data = self.repo_client.get_evidences_map_count(uuid)
+        return {
+            "total_data": total_data,
+            "total_pages": math.ceil(int(total_data) / int(limit) + 0.5),
+            "page": int(page),
+            "items": self.repo_client.get_evidences_map(uuid, limit, page),
+        }
 
     def find_all_users(self, user_email):
         return self.repo_client.find_all_users({"email": user_email})
